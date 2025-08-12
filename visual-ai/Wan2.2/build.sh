b6e6076d96837688d3623d3ba85572bddeed9323
Xiangyu Tian <109123695+xiangyuT@users.noreply.github.com>
2025-08-12 16:10:13 +0800

    Update build.sh

--- visual-ai/Wan2.2/build.sh
+++ visual-ai/Wan2.2/build.sh
@@ -3,4 +3,4 @@ set -x
 export HTTP_PROXY=<your_http_proxy>
 export HTTPS_PROXY=<your_https_proxy>
 
-docker build -f ./docker/Dockerfile . -t llm-scaler-visualai:latest-wan2.1 --build-arg https_proxy=$HTTPS_PROXY --build-arg http_proxy=$HTTP_PROXY
+docker build -f ./docker/Dockerfile . -t llm-scaler-visualai:latest-wan2.2 --build-arg https_proxy=$HTTPS_PROXY --build-arg http_proxy=$HTTP_PROXY
